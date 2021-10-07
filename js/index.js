c551dfd395e5c4b86ab2d88d0e3d053c5a86f319
emi4218 <emi4218@gmail.com>
2021-10-07 13:38:21 +0200

    affichage et js ok

--- js/index.js
+++ js/index.js
@@ -1,5 +1,29 @@
 window.addEventListener('load', () => {
 
+    let move = document.querySelector('.moveCommentaires')
+    let up = document.querySelector('.fa-chevron-up')
+    let down = document.querySelector('.fa-chevron-down')
+    let count = move.childElementCount
+    let i = 0
+    let countDiv = document.querySelector('.count')
+    countDiv.innerHTML = i + 1 + " / " + count
 
+    up.addEventListener('click', event => {
+        if (i + 1 >= count) {
+            return
+        }
+        i++
+        move.style.transform = "translateY(-" + (245 * i) + "px)"
+        countDiv.innerHTML = i + 1 + " / " + count
+    })
+
+    down.addEventListener('click', event => {
+        if (i <= 0) {
+            return
+        }
+        i--
+        move.style.transform = "translateY(-" + (245 * i) + "px)"
+        countDiv.innerHTML = i + 1 + " / " + count
+    })
 
 });
